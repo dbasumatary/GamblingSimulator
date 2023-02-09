50e6749bdeae12bda1218e9c42aaa02255f5d3fb
dbasumatary <basudhondip@gmail.com>
2023-02-09 17:19:30 +0530

    Added: Starting with initial stake and bet

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,7 +1,9 @@
 package com.gamblingsimulator;
-
+/*Starting attributes initialized for the program*/
 public class GamblingSimulator {
+    public static final int EveryDayStake = 100;    //Initializing static variable for total stake
+    public static final int betEveryGame = 1;      //Initializing static variable for small bet
     public static void main(String[] args) {
-        System.out.println(1+2);
+        System.out.println("Welcome To Gambling Simulator Program");
     }
 }
