9df5ad7f1ba2ed9b2331175993cfdb20b457b450
dbasumatary <basudhondip@gmail.com>
2023-02-09 17:27:41 +0530

    Added a method to see the outcome of first bet

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,9 +1,24 @@
 package com.gamblingsimulator;
-/*Starting attributes initialized for the program*/
+/*Making a bet with $1 and observing the outcome*/
 public class GamblingSimulator {
-    public static final int EveryDayStake = 100;    //Initializing static variable for total stake
-    public static final int betEveryGame = 1;      //Initializing static variable for small bet
+    public static final int EveryDayStake = 100;                    //Initializing static variable (UC1)
+    public static final int betEveryGame = 1;                       //Initializing static variable (UC1)
+    /*Creating a new method to see if gambler won or lost (UC2)*/
+    public void gamblingWinLose(){
+        double random = Math.random();
+        int remainingAmount = EveryDayStake;         //Before the bet the remaining amount and total stake is same
+        if (random > 0.5){
+            remainingAmount += betEveryGame;
+            System.out.println("The Gambler won the first bet. He has $" +remainingAmount+ " remaining with him.");
+        }
+        else{
+            remainingAmount -= betEveryGame;
+            System.out.println("The Gambler lost the first bet. He has $" +remainingAmount+ " remaining with him.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator Program");
+        GamblingSimulator simulationUC2 = new GamblingSimulator();                 //Creating a new object
+        simulationUC2.gamblingWinLose();                                           //Calling the method
     }
 }
