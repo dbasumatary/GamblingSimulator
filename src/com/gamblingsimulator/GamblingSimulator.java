6b358025692d62e1fd18d2822399c4223e7a2832
dbasumatary <basudhondip@gmail.com>
2023-02-12 00:46:47 +0530

    Added: Program to check win or loss for a month and by how much

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,15 +1,15 @@
 package com.gamblingsimulator;
-/*Print total amount won or lost in 20 days*/
+/*Print days won or lost each month and by how much*/
 public class GamblingSimulator {
     public static final int EveryDayStake = 100;
     public static final int betEveryGame = 1;
-    public void gamblingTwentyDays(){
-        double upperMargin = 0.5 * EveryDayStake;                // There is 50% margin after win or lose
+    public void monthlyWinOrLose(){
+        double upperMargin = 0.5 * EveryDayStake;                 // There is 50% margin after win or lose
         double lowerMargin = -0.5 * EveryDayStake;
-        double finalAmount = 0;
+        int winDays = 0, lossDays = 0, drawDays = 0;              //Instantiate new variables
         for (int i = 0; i < 20; i++){                                // The gambler plays for 20 days
             double dailyAmount = 0;
-            int betPlayed = 0;                             //Start initial with 0 and then to 100 bets
+            int betPlayed = 0;                                   //Start initial with 0 and then to 100 bets
             /*The dailyAmount has to be in between the two margins of 50% and betPlayed has to be from 0 to 100*/
             while ((dailyAmount < upperMargin) && (dailyAmount > lowerMargin) && (betPlayed < EveryDayStake)){
                 double random = Math.random();
@@ -20,21 +20,24 @@ public class GamblingSimulator {
                     dailyAmount -= betEveryGame;
                 }
             }
-            finalAmount += dailyAmount;                               //Storing the final amount everytime in the loop
+            if (dailyAmount > 0){                                            //Printing the daily amount for wins
+                winDays++;
+                System.out.println("Day "+ i + ": The amount won is $" + dailyAmount);
+            }
+            else if (dailyAmount == 0){                                     //Printing the daily amount for draw
+                drawDays++;
+                System.out.println("Day "+ i +": There is no net gain or loss ");
+            }
+            else if (dailyAmount < 0){                                      //Printing the daily amount for loss
+                lossDays++;
+                System.out.println("Day "+ i + ": The amount lost is $" + Math.abs(dailyAmount));
+            }
         }
-        if (finalAmount > 0){
-            System.out.println("The total amount won in 20 days is $"+ finalAmount);
-        }
-        else if (finalAmount == 0){
-            System.out.println("There is no net gain in last 20 days");
-        }
-        else if (finalAmount < 0){
-            System.out.println("The total amount lost in last 20 days is $"+ Math.abs(finalAmount));
-        }
+        System.out.println("In this month I have won for " + winDays +" days, lost for " + lossDays + " days and drew for " + drawDays + " days");
     }
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator Program");
         GamblingSimulator simulation = new GamblingSimulator();                  //Creating a new object
-        simulation.gamblingTwentyDays();                                            //Calling the method (UC4)
+        simulation.monthlyWinOrLose();                                            //Calling the method (UC5)
     }
 }
