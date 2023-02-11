b72eaca67ca984e99f080ba3676bdaecb4e626fd
dbasumatary <basudhondip@gmail.com>
2023-02-11 23:40:50 +0530

    Added: Program to check win lose amount after 20 days

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,42 +1,40 @@
 package com.gamblingsimulator;
-/*Gambler would resign for the day if won or lose 50% of stake*/
+/*Print total amount won or lost in 20 days*/
 public class GamblingSimulator {
-    public static final int EveryDayStake = 100;                    //Initializing static variable for total stack (UC1)
-    public static final int betEveryGame = 1;                       //Initializing static variable for every bet (UC1)
-    /*(UC2) Creating a new method to see if gambler won or lost*/
-    public void gamblingWinLose(){
-        double random = Math.random();
-        int remainingAmount = EveryDayStake;         //Before the bet the remaining amount and total stake is same
-        if (random > 0.5){
-            remainingAmount += betEveryGame;
-            System.out.println("The Gambler won the first bet. He has $" +remainingAmount+ " remaining with him.");
-        }
-        else{
-            remainingAmount -= betEveryGame;
-            System.out.println("The Gambler lost the first bet. He has $" +remainingAmount+ " remaining with him.");
-        }
-    }
-    /*(UC3) Creating a new method to print that gambler would resign for the day if won or lose 50% of stake*/
-    public void gamblerResign(int margin){
-        int highAmount = EveryDayStake + margin*EveryDayStake/100;            //Upper margin for resigning
-        int lowAmount = EveryDayStake - margin*EveryDayStake/100;             //Lower margin for resigning
-        int availableAmount = EveryDayStake;                                   //Before the bet they will be same
-        while ((availableAmount < highAmount) && (availableAmount > lowAmount)){
-            double random = Math.random();
-            if (random > 0.5){                                           //Using random to check if gambler win or lose
-                availableAmount += betEveryGame;
+    public static final int EveryDayStake = 100;
+    public static final int betEveryGame = 1;
+    public void gamblingTwentyDays(){
+        double upperMargin = 0.5 * EveryDayStake;                // There is 50% margin after win or lose
+        double lowerMargin = -0.5 * EveryDayStake;
+        double finalAmount = 0;
+        for (int i = 0; i < 20; i++){                                // The gambler plays for 20 days
+            double dailyAmount = 0;
+            int betPlayed = 0;                             //Start initial with 0 and then to 100 bets
+            /*The dailyAmount has to be in between the two margins of 50% and betPlayed has to be from 0 to 100*/
+            while ((dailyAmount < upperMargin) && (dailyAmount > lowerMargin) && (betPlayed < EveryDayStake)){
+                double random = Math.random();
+                betPlayed++;
+                if (random > 0.5){
+                    dailyAmount += betEveryGame;
+                }else{
+                    dailyAmount -= betEveryGame;
+                }
             }
-            else{
-                availableAmount -= betEveryGame;
-            }
+            finalAmount += dailyAmount;                               //Storing the final amount everytime in the loop
         }
-        System.out.println("The Gambler resigns for the day. He has $" +availableAmount+ " remaining with him.");
+        if (finalAmount > 0){
+            System.out.println("The total amount won in 20 days is $"+ finalAmount);
+        }
+        else if (finalAmount == 0){
+            System.out.println("There is no net gain in last 20 days");
+        }
+        else if (finalAmount < 0){
+            System.out.println("The total amount lost in last 20 days is $"+ Math.abs(finalAmount));
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator Program");
-        GamblingSimulator simulation = new GamblingSimulator();                 //Creating a new object
-        simulation.gamblingWinLose();                                            //Calling the first method (UC2)
-        GamblingSimulator resignCheck = new GamblingSimulator();
-        resignCheck.gamblerResign(50);                                //Calling the second method (UC3)
+        GamblingSimulator simulation = new GamblingSimulator();                  //Creating a new object
+        simulation.gamblingTwentyDays();                                            //Calling the method (UC4)
     }
 }
