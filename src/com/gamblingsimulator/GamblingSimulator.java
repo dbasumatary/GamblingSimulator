cfc1460b0c37d7cfa029a5f4e9cd1a19f3f629c1
dbasumatary <basudhondip@gmail.com>
2023-02-13 12:00:52 +0530

    Refactored: After winning decision to continue playing or stop

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,13 +1,13 @@
 package com.gamblingsimulator;
-/*Print the luckiest and unluckiest day and by how much*/
+import java.util.Scanner;
+/*Decide whether to continue or stop playing*/
 public class GamblingSimulator {
     public static final int EveryDayStake = 100;
     public static final int betEveryGame = 1;
-    public void luckiestOrUnluckiest(){
+    public void continueOrStop(){
         double upperMargin = 0.5 * EveryDayStake;                 // There is 50% margin after win or lose
         double lowerMargin = -0.5 * EveryDayStake;
-        int luckyDay = 0, unluckyDay = 0;                         //Instantiate new variables
-        double luckyDayAmount = 0, unluckyDayAmount = 0;
+        double totalAmount = 0;
         for (int i = 1; i < 31; i++){                                // The gambler plays for a month
             double dailyAmount = 0;
             int betPlayed = 0;                                   //Start initial with 0 and then to 100 bets
@@ -21,21 +21,34 @@ public class GamblingSimulator {
                     dailyAmount -= betEveryGame;
                 }
             }
-            if (luckyDayAmount < dailyAmount){                         //Checking the condition for lucky days
-                luckyDayAmount = dailyAmount;
-                luckyDay = i;
+            totalAmount += dailyAmount;
+        }
+        /* After win whether he should play or not*/
+        if (totalAmount > 0){
+            System.out.println("The gambler has won $"+ totalAmount+ " last month");
+            Scanner scanner = new Scanner(System.in);
+            //int choice = scanner.nextInt();
+            System.out.println("Would you like to keep playing? Press 1 or 2");
+            System.out.println("1. Continue playing\n2. Stop playing");
+            int choice = scanner.nextInt();
+            if (choice == 1){                                                 // He should play further
+                System.out.println("The gambler has won $"+ totalAmount+ " last month and he should continue to play in this month");
             }
-            else if (unluckyDayAmount > dailyAmount){                  //Checking the condition for unlucky days
-                unluckyDayAmount = dailyAmount;
-                unluckyDay = i;
+            else if (choice == 2){                                               // He should not play further
+                System.out.println("The gambler has won $"+ totalAmount+ " last month and he should stop gambling this month.");
+            }
+            else{
+                System.err.println("Please enter only 1 or 2");
             }
         }
-        System.out.println("The luckiest day for the gambler is day " + luckyDay + " of the month. He won $" + luckyDayAmount);
-        System.out.println("The unluckiest day for the gambler is day " + unluckyDay + " of the month. He lost $" + Math.abs(unluckyDayAmount));
+        /* If he has lost in last month*/
+        else if (totalAmount <= 0){
+            System.out.println("The gambler has lost $" + Math.abs(totalAmount)+ " last month and to avoid further losses he should stop playing now.");
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator Program");
         GamblingSimulator simulation = new GamblingSimulator();                     //Creating a new object
-        simulation.luckiestOrUnluckiest();                                            //Calling the method (UC6)
+        simulation.continueOrStop();                                            //Calling the method (UC7)
     }
 }
